f3d32bbbc289c904340ba1d3053035c763da5fe2
Vincent Demeester <vincent@sbr.pm>
2023-12-20 14:12:30 +0100

    Start at emacs 29.1 🙃
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- org-readwise.el
+++ org-readwise.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vincent Demeester <vincent@sbr.pm>
 ;; Version: 0.1
-;; Package-Requires: ((emacs "27.2") (request "0.3.3") (dash "2.19.1"))
+;; Package-Requires: ((emacs "29.1") (request "0.3.3") (dash "2.19.1"))
 ;; URL: https://github.com/vdemeester/org-readwise
 
 ;; This file is NOT part of GNU Emacs.
